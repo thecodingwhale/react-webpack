34c394f367d03780905c7e267f73b4a7090daf3a
Aldren Terante <aldren.terante@gmail.com>
2018-03-17 22:13:26 +0800

    crate containers folder

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import App from './components/App';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+import Home from './containers/Home';
+import Tasks from './containers/Tasks';
+import Page404 from './components/Page404';
 
 ReactDOM.render((
   <BrowserRouter>
-    <App />
+    <Switch>
+      <Route exact path='/' component={Home}/>
+      <Route path='/tasks' component={Tasks}/>
+      <Route path="*" component={Page404}/>
+    </Switch>
   </BrowserRouter>
 ), document.getElementById('root'));
